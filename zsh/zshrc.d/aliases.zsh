b9fe2c6ae675281157bae617fc9f7e7ddcd71ea6
Calvin Timmer <calvin.timmer@mailbox.org>
2017-06-05 20:53:38 +0200

    Added git aliases

--- zsh/zshrc.d/aliases.zsh
+++ zsh/zshrc.d/aliases.zsh
@@ -1,13 +1,20 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 
+# ls
 LS_OPTS="-l --color=auto"
 alias ls="ls $LS_OPTS"
 alias la="ls -a"
 unset LS_OPTS
 
+# grep
 GREP_OPTS='--color=auto --exclude-dir={.git,.hg,.svn}'
 alias grep="grep $GREP_OPTS"
 alias egrep="egrep $GREP_OPTS"
 alias fgrep="fgrep $GREP_OPTS"
 unset GREP_OPTS
+
+# git
+alias gi="git status"
+alias gg="git gui &"
+alias gk="gitk &"
