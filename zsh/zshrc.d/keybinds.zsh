d4bd24942689123377bd613bae16a9bc7712f24c
Calvin Timmer <calvin.timmer@mailbox.org>
2017-06-05 22:55:46 +0200

    Changed zsh home-key keybinding and added prepend-sudo

--- zsh/zshrc.d/keybinds.zsh
+++ zsh/zshrc.d/keybinds.zsh
@@ -7,37 +7,46 @@ bindkey -e
 typeset -A key
 key=(
 	Home        "${terminfo[khome]}"
+	AltHome     "^[[1;3H"
 	CtrlHome    "^[[1;5H"
 	End         "${terminfo[kend]}"
+	AltEnd      "^[[1;3F"
 	CtrlEnd     "^[[1;5F"
 	Insert      "${terminfo[kich1]}"
 	Delete      "${terminfo[kdch1]}"
 	CtrlBack    "^H"
 	Up          "${terminfo[kcuu1]}"
 	ShiftUp     "^[[1;2A"
+	AltUp       "^[[1;3A"
 	CtrlUp      "^[[1;5A"
 	Down        "${terminfo[kcud1]}"
 	ShiftDown   "^[[1;2B"
+	AltDown     "^[[1;3B"
 	CtrlDown    "^[[1;5B"
 	Right       "${terminfo[kcuf1]}"
 	ShiftRight  "^[[1;2C"
+	AltRight    "^[[1;3C"
 	CtrlRight   "^[[1;5C"
 	Left        "${terminfo[kcub1]}"
 	ShiftLeft   "^[[1;2D"
+	AltLeft     "^[[1;3D"
 	CtrlLeft    "^[[1;5D"
 	PageUp      "${terminfo[kpp]}"
+	AltPgUp     "^[[5;3~"
 	CtrlPgUp    "^[[5;5~"
 	PgDown      "${terminfo[knp]}"
+	AltPgDown   "^[[6;3~"
 	CtrlPgDown  "^[[6;5~"
 	BackTab     "${terminfo[kcbt]}"
 )
 
 # Functions
-function _after-first-word() {
-	zle beginning-of-line
-	zle emacs-forward-word
+function _prepend-sudo {
+  if [[ $BUFFER != "sudo "* ]]; then
+    BUFFER="sudo $BUFFER"; CURSOR+=5
+  fi
 }
-zle -N _after-first-word
+zle -N _prepend-sudo
 
 function _replace-first-word() {
 	zle beginning-of-line
@@ -47,7 +56,7 @@ zle -N _replace-first-word
 
 
 # Movement
-bindkey ${key[Home]}        _after-first-word
+bindkey ${key[Home]}        beginning-of-line
 bindkey ${key[End]}         end-of-line
 bindkey ${key[Up]}          up-line-or-search
 bindkey ${key[Down]}        down-line-or-search
@@ -55,6 +64,7 @@ bindkey ${key[CtrlRight]}   forward-word
 bindkey ${key[CtrlLeft]}    backward-word
 
 # Modification
+bindkey "${key[AltHome]}"   _prepend-sudo
 bindkey "${key[CtrlHome]}"  _replace-first-word
 bindkey "${key[CtrlBack]}"  backward-kill-word
 
