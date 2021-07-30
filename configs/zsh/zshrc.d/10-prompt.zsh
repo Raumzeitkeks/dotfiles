8a7e02f714f6a48ece71b0f0250fbb14311e606e
Calvin Ruocco <calvin@ruoc.co>
2021-07-30 10:15:10 +0200

    Add elapsed time and clock to zsh prompt

--- configs/zsh/zshrc.d/10-prompt.zsh
+++ configs/zsh/zshrc.d/10-prompt.zsh
@@ -1,6 +1,26 @@
-# VCS information
+zmodload zsh/datetime
 autoload -Uz vcs_info
-precmd() vcs_info
+
+preexec () {
+  # capture execution start time
+  start_time=${EPOCHREALTIME}
+}
+
+precmd () {
+  # measure and format execution time
+  unset elapsed
+  if (( start_time )); then
+    local -F elapsed_time_float=$(( EPOCHREALTIME - start_time ))
+    local -i elapsed_time_int=${elapsed_time_float}
+    local -i h=$(( elapsed_time_int/3600 ))
+    local -i m=$(( (elapsed_time_int/60)%60 ))
+    local -F s=$(( elapsed_time_float%60 ))
+    LC_NUMERIC=C printf -v elapsed '%02d:%02d:%06.3f' $h $m $s
+    unset start_time
+  fi
+  # record vcs information
+  vcs_info
+}
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' max-exports 5
@@ -13,8 +33,10 @@ zstyle ':vcs_info:git*' actionformats '%r' '%S' '%b' '%c' '%a' '%m'
 # Enable command substitution in promps
 setopt PROMPT_SUBST
 
-(){
-  local err='%B%(?,%F{green}✔%f,%F{red}✘%f)%b '
+() {
+  local info='%B%(?,%F{green}✔,%F{red}✘)%b${elapsed:+ $elapsed}%f '
+  local clock='─── %F{cyan}%*%f ───'
+  local newline=$'\n'
   local basepath='%F{blue}${${vcs_info_msg_0_:+${${(%):-%~}%$vcs_info_msg_0_${${vcs_info_msg_1_#[.]}:+/$vcs_info_msg_1_}}}:-${${${(%):-%~}#[~]}:+${${(%):-%~}%/*}/}}%f'
   local coolpath='%F{blue}${${vcs_info_msg_0_:+${${${vcs_info_msg_1_#[.]}:+%U$vcs_info_msg_0_%u${${(%):-/$vcs_info_msg_1_}%/*}/%B${vcs_info_msg_1_##*/}%b}:-%U%B$vcs_info_msg_0_%b%u}}:-%B${${(%):-%~}##*/}%b}%f'
   local cwd="$basepath$coolpath "
@@ -22,7 +44,7 @@ setopt PROMPT_SUBST
   local arrow='%B%F{black}%(!.#.>>)%f%b '
 
   # main prompt
-  PROMPT="$err$cwd$vcs$arrow"
+  PROMPT="$info$clock$newline$cwd$vcs$arrow"
 
   # continuation prompt
   PROMPT2='%B%_${${(%):-%_}:+ }... %b'
