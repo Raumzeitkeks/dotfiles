8e8584f3765da5a7bc157e58b73c46dcedc3e103
Ruocco, Calvin <Calvin.Ruocco@vector.com>
2023-05-04 14:21:18 +0200

    Simplify zsh prompt

--- configs/zsh/zshrc.d/10-prompt.zsh
+++ configs/zsh/zshrc.d/10-prompt.zsh
@@ -1,5 +1,4 @@
 zmodload zsh/datetime
-autoload -Uz vcs_info
 
 preexec () {
   # capture execution start time
@@ -9,6 +8,7 @@ preexec () {
 precmd () {
   # measure and format execution time
   unset elapsed
+  elapsed="00:00:00.000"
   if (( start_time )); then
     local -F elapsed_time_float=$(( EPOCHREALTIME - start_time ))
     local -i elapsed_time_int=${elapsed_time_float}
@@ -18,33 +18,20 @@ precmd () {
     LC_NUMERIC=C printf -v elapsed '%02d:%02d:%06.3f' $h $m $s
     unset start_time
   fi
-  # record vcs information
-  vcs_info
 }
-
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' max-exports 5
-zstyle ':vcs_info:git*' check-for-staged-changes true
-zstyle ':vcs_info:git*' stagedstr '*'
-zstyle ':vcs_info:git*' patch-format '%p [%n|%c]'
-zstyle ':vcs_info:git*' formats       '%r' '%S' '%b' '%c'
-zstyle ':vcs_info:git*' actionformats '%r' '%S' '%b' '%c' '%a' '%m'
 
 # Enable command substitution in promps
 setopt PROMPT_SUBST
 
 () {
-  local info='%B%(?,%F{green}✔,%F{red}✘)%b${elapsed:+ $elapsed}%f '
-  local clock='─── %F{cyan}%*%f ───'
-  local newline=$'\n'
-  local basepath='%F{blue}${${vcs_info_msg_0_:+${${(%):-%~}%$vcs_info_msg_0_${${vcs_info_msg_1_#[.]}:+/$vcs_info_msg_1_}}}:-${${${(%):-%~}#[~]}:+${${(%):-%~}%/*}/}}%f'
-  local coolpath='%F{blue}${${vcs_info_msg_0_:+${${${vcs_info_msg_1_#[.]}:+%U$vcs_info_msg_0_%u${${(%):-/$vcs_info_msg_1_}%/*}/%B${vcs_info_msg_1_##*/}%b}:-%U%B$vcs_info_msg_0_%b%u}}:-%B${${(%):-%~}##*/}%b}%f'
-  local cwd="$basepath$coolpath "
-  local vcs='${vcs_info_msg_0_:+%B[%b%F{magenta\}$vcs_info_msg_2_${vcs_info_msg_3_:+ $vcs_info_msg_3_}${vcs_info_msg_4_:+ ($vcs_info_msg_4_${vcs_info_msg_5_:+ | $vcs_info_msg_5_})}%f%B]%b }'
+  local rc='%(?,%F{green}[---],%F{red}[${(l:3::0:)${(%):-%?}}])%f '
+  local time='%B%F{yellow}$elapsed%f%b '
+  local cwd='%F{blue}%~%f '
+  local git='$(__git_ps1 "[%s] ")'
   local arrow='%B%F{black}%(!.#.>>)%f%b '
 
   # main prompt
-  PROMPT="$info$clock$newline$cwd$vcs$arrow"
+  PROMPT="$rc$time$cwd$git$arrow"
 
   # continuation prompt
   PROMPT2='%B%_${${(%):-%_}:+ }... %b'
@@ -55,4 +42,3 @@ setopt PROMPT_SUBST
   # correction prompt
   SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [yNae]? '
 }
-
